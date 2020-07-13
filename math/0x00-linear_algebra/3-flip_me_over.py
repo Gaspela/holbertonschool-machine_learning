2a7156ab704622d34886d82967dac7f1d36783b2
Gaspela04 <samirmillanorozco@hotmail.com>
2020-07-13 20:11:51 +0000

    Transpose matrix for diagonal zero

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" returns the transpose of a 2D matrix """
+
+
+def matrix_transpose(matrix):
+    """ the transpose of a 2D matrix """
+    return [[row[zero] for row in matrix] for zero in range(len(matrix[0]))]
+    """  t_matrix = zip(*matrix)
+    for row in t_matrix:
+        print(row) """
+        
