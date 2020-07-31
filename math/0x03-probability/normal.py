bfaccda2b40acea3194ce9b511580ff89ae0dbc0
Gaspela04 <samirmillanorozco@hotmail.com>
2020-07-31 16:27:45 +0000

    Resolve pdf,cdf

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -34,3 +34,12 @@ class Normal:
 
     def pdf(self, x):
         """ Calculates the value of the PDF for a given x-value """
+        return (pow(2.7182818285, ((x - self.mean) ** 2 /
+                                   (-2 * self.stddev ** 2))) /
+                (2 * 3.1415926536 * self.stddev ** 2) ** .5)
+
+    def cdf(self, x):
+        """ Calculates the value of the CDF for a given x-value """
+        e = (x - self.mean) / (self.stddev * 2 ** .5)
+        return (1 + (e - e ** 3 / 3 + e ** 5 / 10 - e ** 7 /
+                     42 + e ** 9 / 216) * 2 / 3.1415926536 ** .5) / 2
