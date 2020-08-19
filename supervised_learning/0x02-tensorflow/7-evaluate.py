4788924c642831d5be35e0593399010af4ce4d2a
Gaspela04 <samirmillanorozco@hotmail.com>
2020-08-19 17:44:30 +0000

    Eval

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -12,14 +12,18 @@ def evaluate(X, Y, save_path):
     You are not allowed to use tf.saved_model.
     """
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver = tf.train.import_meta_graph('{}.meta'.format(save_path))
         saver.restore(sess, save_path)
-        x = tf.get_collection("x")[0]
-        y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")[0]
-        accuracy = tf.get_collection("accuracy")[0]
-        loss = tf.get_collection("loss")[0]
-        cost = sess.run(loss, feed_dict={x: X, y: Y})
-        accu = sess.run(accuracy, feed_dict={x: X, y: Y})
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
 
-        return accu, cost
+        y_pred_evaluate, loss_evaluate,
+        accuracy_evaluate = sess.run((y_pred, loss,
+                                      accuracy),
+                                     feed_dict={x: X,
+                                                y: Y})
+
+    return y_pred_evaluate, accuracy_evaluate, loss_evaluate
