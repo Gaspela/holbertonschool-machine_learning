6024cb48f88623ea1d8fb21e802a06098cc2e22b
Gaspela04 <samirmillanorozco@hotmail.com>
2020-08-24 04:30:10 +0000

    batchnormal

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -19,5 +19,12 @@ def create_batch_norm_layer(prev, n, activation):
     you should use an epsilon of 1e-8.
     Returns: a tensor of the activated output for the layer.
     """
-
-
+    kernal = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    model = tf.layers.Dense(units=n, name="layer", kernel_initializer=kernal)
+    X = model(prev)
+    beta = tf.Variable(tf.constant(0.0, shape=[n]), trainable=True)
+    gamma = tf.Variable(tf.constant(1.0, shape=[n]), trainable=True)
+    mean, variance = tf.nn.moments(X, [0])
+    norm_z = tf.nn.batch_normalization(X, mean, variance, offset=beta,
+                                       scale=gamma, variance_epsilon=1e-8)
+    return activation(norm_z)
