a55021c85ff4eca28b8f5ce04b5187be9a36e42d
Samir millan <samirmillanorozco@hotmail.com>
2020-10-23 17:12:39 +0000

    Type matrix, solve error (List index out of range)

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -8,8 +8,8 @@ def determinant(matrix):
     """
     matrix is a list of lists whose determinant should be calculated
     """
-    if type(matrix) is not list or len(matrix) is 0:
-        raise TypeError('matrix must be a list of lists')
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
 
     for row in matrix:
         if not isinstance(row, list):
@@ -21,7 +21,7 @@ def determinant(matrix):
     if len(matrix) is not len(matrix[0]):
         raise ValueError('matrix must be a square matrix')
 
-    if len(matrix) == 1:
+    if len(matrix) is 1 and len(matrix[0]) is 1:
         return matrix[0][0]
 
     detres = 0
