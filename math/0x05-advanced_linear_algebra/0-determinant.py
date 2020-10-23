b57f158e6b1d3d572013e14bdd56e0c06a942e9a
Samir millan <samirmillanorozco@hotmail.com>
2020-10-23 16:58:01 +0000

    Detres

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+that calculates the determinant of a matrix
+"""
+
+
+def determinant(matrix):
+    """
+    matrix is a list of lists whose determinant should be calculated
+    """
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) is 1 and len(matrix[0]) is 0:
+        return 1
+
+    if len(matrix) is not len(matrix[0]):
+        raise ValueError('matrix must be a square matrix')
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    detres = 0
+    for i, j in enumerate(matrix[0]):
+        row = [r for r in matrix[1:]]
+        tmp = []
+
+        for r in row:
+            aux = []
+            for c in range(len(matrix)):
+                if c != i:
+                    aux.append(r[c])
+            tmp.append(aux)
+
+        detres += j * (-1) ** i * determinant(tmp)
+
+    return detres
