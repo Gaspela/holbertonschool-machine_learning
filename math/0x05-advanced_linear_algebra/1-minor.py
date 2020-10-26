f4839097e25db08de83a80db9977c0d06da3f0b0
Samir millan <samirmillanorozco@hotmail.com>
2020-10-26 16:27:44 +0000

    Fix not list the lists

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -8,24 +8,26 @@ def determinant(matrix):
     """
     matrix is a list of lists whose determinant should be calculated
     """
-    if type(matrix) is not list or len(matrix) is 0:
-        raise TypeError('matrix must be a list of lists')
-
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError('matrix must be a list of lists')
-
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError("matrix must be a list of lists")
+    if not isinstance(matrix, list) or len(matrix) is 0:
+        raise TypeError("matrix must be a list of lists")
 
     if len(matrix) is 1 and len(matrix[0]) is 0:
         return 1
 
-    if len(matrix) is not len(matrix[0]):
-        raise ValueError('matrix must be a square matrix')
+    for r in matrix:
+        if not isinstance(r, list):
+            raise TypeError("matrix must be a list of lists")
+
+    for r in matrix:
+        if len(r) != len(matrix):
+            raise ValueError('matrix must be a square matrix')
 
     if len(matrix) is 1 and len(matrix[0]) is 1:
         return matrix[0][0]
+
+    if len(matrix) is 2 and len(matrix[0]) is 2:
+        det = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+        return det
 
     detres = 0
     for i, j in enumerate(matrix[0]):
@@ -48,14 +50,15 @@ def minor(matrix):
     """
     matrix is a list of lists whose minor matrix should be calculated
     """
-    if type(matrix) is not list or len(matrix) is 0:
-        raise TypeError('matrix must be a list of lists')
-
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a list of lists")
 
-    if matrix is [[]]:
-        raise ValueError('matrix must be a non-empty square matrix')
+    if not matrix:
+        raise TypeError("matrix must be a list of lists")
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a list of lists")
 
     for row in matrix:
         if len(matrix) != len(row):
