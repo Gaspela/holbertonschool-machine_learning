60be37fef18cade216b39c0ebd3425d2ac509088
Samir millan <samirmillanorozco@hotmail.com>
2021-01-09 01:46:00 +0000

    transformer

--- supervised_learning/0x11-attention/11-transformer.py
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -1,1 +1,46 @@
 #!/usr/bin/env python3
+"""
+Transformer Network
+"""
+
+import tensorflow as tf
+Encoder = __import__('9-transformer_encoder').Encoder
+Decoder = __import__('10-transformer_decoder').Decoder
+
+
+class Transformer(tf.keras.Model):
+    """
+    transformer network
+    """
+
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
+        """
+        N - the number of blocks in the encoder and decoder
+        dm - the dimensionality of the model
+        h - the number of heads
+        hidden - the number of hidden units in the fully connected layers
+        input_vocab - the size of the input vocabulary
+        target_vocab - the size of the target vocabulary
+        max_seq_input - the maximum sequence length possible for the input
+        max_seq_target - the maximum sequence length possible for the target
+        drop_rate - the dropout rate
+        """
+        super().__init__()
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab,
+                               max_seq_input, drop_rate)
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab,
+                               max_seq_target, drop_rate)
+        self.linear = tf.keras.layers.Dense(target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask,
+             look_ahead_mask, decoder_mask):
+        """
+        Call method call()
+        """
+        enco_output = self.encoder(inputs, training, encoder_mask)
+        deco_output = self.decoder(target, enco_output, training,
+                                   look_ahead_mask, decoder_mask)
+        output = self.linear(deco_output)
+
+        return (output)
