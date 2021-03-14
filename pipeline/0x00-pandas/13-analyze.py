e5466dec4d1c52bbb29de88a34c045f85c82f345
Samir millan <samirmillanorozco@hotmil.com>
2021-03-14 03:35:53 +0000

    PandasP

--- pipeline/0x00-pandas/13-analyze.py
+++ pipeline/0x00-pandas/13-analyze.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Slice
+Analyze
 """
 import pandas as pd
 
 
 from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df = df.iloc[::60, 2:6]
+stats = df.describe().drop(["Timestamp"], axis=1)
 
-print(df.tail())
+print(stats)
--- pipeline/0x00-pandas/3-rename.py
+++ pipeline/0x00-pandas/3-rename.py
@@ -10,4 +10,5 @@ df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df = df.rename(columns={"Timestamp": "Datetime"})
 df["Datetime"] = pd.to_datetime(df["Datetime"], unit='s')
 df = df.loc[:, ["Datetime", "Close"]]
+
 print(df.tail())
