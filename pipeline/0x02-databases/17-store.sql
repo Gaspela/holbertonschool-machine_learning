d3bd2dc37f2ca604576fda438e968867d83dd3d1
Samir millan <samirmillanorozco@hotmil.com>
2021-03-17 17:34:52 +0000

    Sql, Uniq users, fans, valid Email

---
+++ pipeline/0x02-databases/17-store.sql
@@ -0,0 +1,6 @@
+-- Buy buy buy
+DELIMITER //
+CREATE TRIGGER transaction__ AFTER INSERT ON orders FOR EACH ROW
+BEGIN
+UPDATE items SET quantity = quantity - NEW.number WHERE NEW.item_name = name;
+END//
