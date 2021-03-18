79c8ef1f6ccae4345e36c72d866dbe5baa3d5f12
Samir millan <samirmillanorozco@hotmil.com>
2021-03-18 01:47:38 +0000

    Avg score

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,8 +1,8 @@
 -- Average score
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT)
-    BEGIN
-        UPDATE users SET average_score = (SELECT AVG(score) FROM corrections INNER JOIN users ON corrections.user_id = users.id WHERE corrections.user_id=user_id);
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+    SET avg_score = (SELECT AVG(score) FROM corrections INNER JOIN users ON corrections.user_id = users.id WHERE corrections.user_id=user_id);
 END //
 DELIMITER ;
